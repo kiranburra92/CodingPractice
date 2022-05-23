8a9df07d10724bad3614e04640546be2931016f6
Saikiran goud Burra <saikiran.af72@gmail.com>
2022-05-23 18:49:37 -0500

    leetcode Unique Email Addresses

--- src/test/java/com/burra/practice/StackUsingQueuesTest.java
+++ src/test/java/com/burra/practice/StackUsingQueuesTest.java
@@ -4,16 +4,16 @@ import org.junit.Test;
 
 public class StackUsingQueuesTest {
 
-  StackUsingQueues sq = new StackUsingQueues();
+    StackUsingQueues sq = new StackUsingQueues();
 
-  @Test
-  public void simpleTest() {
-    sq.addToStack(1);
-    sq.addToStack(3);
-    sq.addToStack(5);
+    @Test
+    public void simpleTest() {
+        sq.addToStack(1);
+        sq.addToStack(3);
+        sq.addToStack(5);
 
-    System.out.println(sq.addToStack(7));
+        System.out.println(sq.addToStack(7));
 
-    System.out.println(sq.removeFromStack());
-  }
+        System.out.println(sq.removeFromStack());
+    }
 }
